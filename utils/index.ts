9c1c976e3a61868cad5fb7aedca340ac1bef99ee
Metin Keleş <metinkeles677@gmail.com>
2024-02-09 14:25:39 +0300

    Add fetchCars and calculateCarRent functions for car data retrieval and rental calculation

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "652d2ac94amsh47d1ab7ce92529cp1ff88bjsn032b5c1b400b",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
