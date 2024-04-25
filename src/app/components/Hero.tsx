0999c7ce80ddc0d7eb401f0b252d2800e121ebe2
Metin Keleş <metinkeles677@gmail.com>
2024-04-25 09:40:41 +0300

    Update Hero component: Refactor and add car count feature

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 import { CustomButton } from "../components";
 
-const Hero = () => {
+const Hero = ({ carCount }: int) => {
   const handleScroll = () => {};
 
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'>
-          Find, book, rent a car—quick and super easy!
+          Find, book, buy a car; fast and extremely easy!
         </h1>
 
         <p className='hero__subtitle'>
-          Streamline your car rental experience with our effortless booking
-          process.
+          Simplify your car buying experience with our hassle-free booking
+          service process.
         </p>
 
         <CustomButton
@@ -25,12 +25,69 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className='hero__image-container'>
+      {/* <div className='hero__image-container'>
         <div className='hero__image '>
           <Image src='/hero.png' alt='hero' fill className='object-contain' />
         </div>
 
         <div className='hero__image-overlay' />
+      </div>*/}
+      <div className='xl:flex-[1.5] flex justify-center items-center w-full xl:h-screen'>
+        <div className='grid grid-cols-2 gap-3'>
+          {/* <Image
+            src='/hero-clover-car-1.jpg'
+            alt='hero'
+            width={350}
+            height={350}
+            className='object-fill min-w-full min-h-full rounded-full rounded-br-none'
+          /> */}
+
+          <div className='flex items-center justify-center bg-primary-blue rounded-full rounded-br-none text-primary-blue-100'>
+            <div className='relative'>
+              <h1 className='hero__title'>{carCount + 60}</h1>
+              <span className='absolute lg:top-3 top-0 lg:left-20 left-14 text-xl'>
+                +
+              </span>
+            </div>
+            <p className='text-[27px] mt-5 ml-2 '>Total Car</p>
+          </div>
+
+          <div className='group relative'>
+            <div className='overflow-hidden rounded-lg transform transition-transform duration-300 group-hover:scale-105'>
+              <Image
+                src='/hero-clover-car-3.jpeg'
+                alt='hero'
+                width={350}
+                height={350}
+                className='object-fill lg:min-h-60 min-h-44 rounded-full rounded-bl-none'
+              />
+            </div>
+          </div>
+
+          <div className='group relative'>
+            <div className='overflow-hidden rounded-lg transform transition-transform duration-300 group-hover:scale-105'>
+              <Image
+                src='/hero-clover-car-2.jpg'
+                alt='hero'
+                width={350}
+                height={350}
+                className='object-fill lg:min-h-60 min-h-44 rounded-full rounded-tr-none'
+              />
+            </div>
+          </div>
+
+          <div className='group relative'>
+            <div className='overflow-hidden rounded-lg transform transition-transform duration-300 group-hover:scale-105'>
+              <Image
+                src='/hero-clover-car-4.jpg'
+                alt='hero'
+                width={350}
+                height={350}
+                className='object-fill lg:min-h-60 min-h-44 rounded-full rounded-tl-none'
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
