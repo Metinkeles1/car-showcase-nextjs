4105b15d5db9acbbdfc7ce79ca69d9a9f581ef6b
Metin Keleş <metinkeles677@gmail.com>
2024-04-25 09:39:55 +0300

    Split About component into two separate components

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,37 +1,66 @@
 import React from "react";
 import Image from "next/image";
+import { GiCarKey } from "react-icons/gi";
+import { MdOutlineShowChart } from "react-icons/md";
+import { LuHeartHandshake } from "react-icons/lu";
 
 const About = () => {
   return (
-    <div className='bg-primary-blue text-white'>
-      <div className='about flex justify-between'>
-        <div className='flex-1 xl:py-36 py-12 padding-x '>
-          <h1 className='about__title'>About Me</h1>
+    <div className='container mx-auto px-4 xl:px-0'>
+      <h1 className='text-4xl font-semibold text-center my-8'>
+        About Our Car Sales
+      </h1>
+      <p className='text-lg text-gray-700 text-center mb-8'>
+        At Our Car Sales, we prioritize your car buying experience with
+        top-quality vehicles and exceptional services. Contact us today to find
+        your dream car.
+      </p>
 
-          <p className='about__subtitle '>
-            At Rent a Car, we prioritize your travel experiences. With
-            top-quality car rental services, we aim to make your journeys
-            enjoyable and hassle-free. Understanding your needs is key, and we
-            strive to offer the most suitable vehicles and services. Customer
-            satisfaction is paramount to us, and we operate with principles of
-            reliability, transparency, and quality. We take pride in
-            facilitating your travel plans, offering personalized solutions, and
-            creating unforgettable memories. Our diverse range of beautiful
-            vehicles and competitive prices ensure we serve you at all times.
-            Contact us today to experience the Rent a Car difference and make
-            your journeys truly exceptional. We're here to make every moment
-            unforgettable
-          </p>
+      <div className='flex justify-center mb-12'>
+        <div className='relative w-full h-96'>
+          <Image
+            src='/about-car-blue-3.png'
+            alt='about'
+            layout='fill'
+            objectFit='contain'
+          />
         </div>
-        <div className='about__image-container xl:py-36 pb-36'>
-          <div className='about__image'>
-            <Image
-              src='/about-img.png'
-              alt='about'
-              fill
-              className='object-contain'
-            />
-            <div className='about__image-overlay' />
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-3 lg:gap-16 gap-10 xl:px-28'>
+        <div className='flex flex-col items-center justify-center'>
+          <GiCarKey className='text-4xl text-primary-blue mb-4' />
+          <div className='text-center lg:h-48'>
+            <h2 className='text-xl font-semibold mb-2'>Browse Our Cars</h2>
+            <p className='text-center text-gray-700'>
+              Explore our wide selection of high-quality cars. Find the perfect
+              vehicle to suit your needs and preferences.
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <MdOutlineShowChart className='text-4xl text-primary-blue mb-4' />
+          <div className='text-center lg:h-48'>
+            <h2 className='text-xl font-semibold mb-2'>
+              Easy Financing Options
+            </h2>
+            <p className='text-center text-gray-700'>
+              Flexible financing options tailored to your budget and
+              preferences. Our finance experts will guide you to find the best
+              solution.
+            </p>
+          </div>
+        </div>
+        <div className='flex flex-col items-center justify-center'>
+          <LuHeartHandshake className='text-4xl text-primary-blue mb-4' />
+          <div className='text-center lg:h-48'>
+            <h2 className='text-xl font-semibold mb-2'>
+              Exceptional Customer Service
+            </h2>
+            <p className='text-center text-gray-700'>
+              Our commitment to customer satisfaction sets us apart. Experience
+              personalized service and a positive buying journey.
+            </p>
           </div>
         </div>
       </div>
